1229ee9363ee7dfa219ca7e2836de37e0fc49dc7
steelbrain <de.steelbrain@gmail.com>
2016-04-16 23:32:36 -0700

    :white_check_mark: Write specs for main module

---
+++ spec/main-spec.js
@@ -0,0 +1,18 @@
+'use babel'
+
+/* @flow */
+
+import { it } from './helpers'
+import extify from '../'
+
+describe('Extify', function() {
+  it('works if the path provided exists on the FS', async function() {
+    expect(await extify(__filename)).toBe(__filename)
+  })
+  it('tries to append ext in front if path is absolute', async function() {
+    expect(await extify(__filename.slice(0, -3), '', '.rar;.js')).toBe(__filename)
+  })
+  it('tries to find the file if path is not absolute', async function() {
+    expect(await extify('main-spec', __dirname, '.js')).toBe(__filename)
+  })
+})
